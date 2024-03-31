08f98bddf5363fe9749710f83de914c7606f95e0
773 <lizzh03@sjtu.edu.cn>
2024-03-31 16:40:03 +0800

    hands on exercise for week 5

--- HOE-5/script.m
+++ HOE-5/script.m
@@ -40,3 +40,23 @@ hold off
 xlabel('T/IJI')
 ylabel('M^2')
 title('M^2 per spin in square lattice')
+%% comparison of 2 by 2 square in energy
+x=0.01:0.01:4;
+y = -8*(1-exp(-16./x))./(1+6*exp(-8./x)+exp(-16./x));
+scatter(data2(:,1),data2(:,2)/4)
+hold on
+plot(x,(8+y)/4)
+hold off
+xlabel('T/IJI')
+ylabel('H/J')
+title('energy per spin in square lattice')
+%% comparison of 2 by 2 square inmagnetization
+x=0.01:0.01:4;
+y = (1+exp(-8./x))./(1+6*exp(-8./x)+exp(-16./x));
+scatter(data2(:,1),data2(:,4)/16)
+hold on
+plot(x,y)
+hold off
+xlabel('T/IJI')
+ylabel('M^2')
+title('M^2 per spin in square lattice')
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # CP2024
 
 editor:李宗臻-522072910059
+
+Exercise of Week #5    >>    CP2024/HOE-5/
+
+在SquareSystemTest中对计算E、M、和配分函数的函数进行了测试
+
